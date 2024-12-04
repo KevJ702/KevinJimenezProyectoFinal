a655e00770bff44a5cb1b2df982a93e70b9fbb03
Kevj702@outlook.com <Kevj702@outlook.com>
2024-12-03 20:20:01 -0400

    Prueba Selenium agregada

---
+++ Seleniumtest/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Interactions;
+
+namespace Seleniumtest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            IWebDriver driver = new FirefoxDriver();
+
+            driver.Navigate().GoToUrl("https://localhost:44317/Inicio.aspx");
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(100000);
+           
+            IWebElement input2 = driver.FindElement(By.Id("LinkButton7"));
+            input2.Click();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(15000000);
+            IWebElement input = driver.FindElement(By.Id("LinkButton1"));
+            input.Click();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+            ITakesScreenshot screenshotdriver = (ITakesScreenshot)driver;
+            Screenshot screenshot = screenshotdriver.GetScreenshot();
+            screenshot.SaveAsFile("screenshot inicio.png");
+
+
+
+
+
+        }
+    }
+}
